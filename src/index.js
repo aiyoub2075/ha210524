5d9ebff8b1386d1094cfd9551659fbcdd9ef4c16
aiyoub2075 <aiyoub.reichert@edu.techstarter.de>
2024-05-22 12:14:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 const add = (a, b) => a + b;
 
@@ -15,23 +14,23 @@ const App = () => (
   <>
     <h1 className="kopftext">Aiyoub Hausaufgabe 21.May</h1>
     <p className="paragraf">Hallo Sara, Hallo Hubertous!</p>
-    
-    <h3 style={{ color: 'black', fontSize: '50px' }}>
+
+    <h3 style={{ color: "black", fontSize: "50px" }}>
       Addieren von 10 und 20 = {add(10, 20)}
     </h3>
-    
-    <h3 style={{ color: 'green', fontSize: '40px' }}>
+
+    <h3 style={{ color: "green", fontSize: "40px" }}>
       Quadratwurzel von 16: {sqrt(16)}
     </h3>
-    
-    <p style={{ color: 'black', fontSize: '30px' }}>
+
+    <p style={{ color: "black", fontSize: "30px" }}>
       Zufällige Zahl zwischen 1 und 100: {randomNumber}
     </p>
-    
-    <p style={{ color: 'green', fontSize: '20px' }}>
+
+    <p style={{ color: "green", fontSize: "20px" }}>
       Aktuelles Datum: {currentDate}
     </p>
   </>
 );
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
